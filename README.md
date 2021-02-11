de2b6854c708682f460b8e4a65c96a39e5d3b950
nikita <tebloev.nikita@mail.ru>
2021-02-11 09:47:23 +0800

    second and third tasks added, inner reorganisation

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-<h1>Первые задания(python)</h1>
+<h1>python</h1>
 <b>first.py</b> - Числа Армстронга<br>
 <b>second.py</b> - Дружественные числа<br>
 <b>third.py</b> - Обрученные числа<br>
 
-<h1>Первые задания(с++)</h1>
-<b>first.cpp</b> - Числа Армстронга<br>
+<h1>с++</h1>
+<b>[first.cpp](https://github.com/Azure/azure-content/blob/master/contributor-guide/contributor-guide-index.md)</b> - Числа Армстронга<br>
 <b>second.cpp</b> - Дружественные числа<br>
 <b>third.cpp</b> - Обрученные числа<br>
---
+++ c++/second.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int devsSum(int num){
+  int s = 0;
+  for(int i = 1; i < num; i++){
+    if(num % i == 0){
+      s += i;
+    }
+  }
+  return s;
+}
+vector<int> findFrends(int limit){
+  vector<int> myVector;
+  for(int i = 1; i < limit; i++){
+    if(find(myVector.begin(), myVector.end(), i) == myVector.end()){
+      int temp = devsSum(i);
+      if(i == devsSum(temp) && i != temp){
+        myVector.push_back(i);
+        myVector.push_back(temp);
+      }
+    }
+  }
+  return myVector;
+}
+void pairedOutput(const vector<int>& vec){
+  for(int i = 0; i < vec.size(); i += 2){
+    cout << vec[i] << ", " << vec[i+1] << endl;
+  }
+}
+int main() {
+  pairedOutput(findFrends(10000));
+}
